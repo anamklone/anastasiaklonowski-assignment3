e803589b53d3242db100a7278ef5333d8e9a94bf
anamklone <amk226@pitt.edu>
2016-11-10 19:41:11 -0500

    Created ChildClass with constructor, getter and setter methods, and toString() method

--- ChildClass.php
+++ ChildClass.php
@@ -1,6 +1,33 @@
 <?php 
-	// this file will extend PArentClass.php
+	// this file will extend ParentClass.php
 
 	class ChildClass extends ParentClass {
-		
+		private $location;
+    private $time;
+    
+    public function __construct($l, $t) {
+      $this->location = $l;
+      $this->time = $t;
+    }
+    
+    public function getLocation() {
+      return $this->location;
+    }
+    
+    public function setLocation($l) {
+      $this->location = $l;
+    }
+    
+    public function getTime() {
+      return $this->time;
+    }
+    
+    public function setTime($t) {
+      $this->time = $t;
+    }
+    
+    public function __toString() {
+      return parent::__toString() . $this->getLocation() . "<br>" . $this->getTime() . "<br>";
+    }
 	}
+?>
