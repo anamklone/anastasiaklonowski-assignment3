bfebd1a4b7d9037866cb075b101111eb76a3b30f
anamklone <amk226@pitt.edu>
2016-11-10 19:30:17 -0500

    Created ParentClass with constructor and getter and setter methods

--- ParentClass.php
+++ ParentClass.php
@@ -2,5 +2,37 @@
 	// This is the file for the parent class
 
 	class ParentClass {
-		
-	}
+    private $classNumber;
+    private $className;
+    private $professor;
+    
+    public function __construct($num, $name, $prof) {
+      $this->classNumber = $num;
+      $this->className = $name;
+      $this->professor = $prof;
+    }
+    
+    public function getClassNumber() {
+      return $this->classNumber;
+    }
+    
+    public function setClassNumber($num) {
+      $this->classNumber = $num;
+    }
+    
+    public function getClassName() {
+      return $this->className;
+    }
+    
+    public function setClassName($name) {
+      $this->className = $name;
+    }
+    
+    public function getProfessor() {
+      return $this->professor;
+    }
+    
+    public function setProfessor($prof) {
+      $this->professor = $prof;
+    }
+  }
